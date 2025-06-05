4577fd47f1efb88ffab862c8182993728211890b
Raj-Lok <rajlok.73@gmail.com>
2025-06-05 12:00:24 +0530

    Create factorial.hpp

---
+++ factorial.hpp
@@ -0,0 +1,10 @@
+// factorial.hpp
+#ifndef FACTORIAL_HPP
+#define FACTORIAL_HPP
+
+#include <stdexcept>
+
+// Function to calculate factorial of a number
+unsigned long long factorial(int n);
+
+#endif // FACTORIAL_HPP
